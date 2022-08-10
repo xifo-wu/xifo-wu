cc3f6d60ea973a69330102cbcbbd8d6bf9bd8492
Seif Wu <seif.wuxf@gmail.com>
2022-08-10 14:07:11 +0800

    feat: add type-challenges 3312-easy-parameters

---
+++ type-challenges/src/3312-easy-parameters.ts
@@ -0,0 +1,48 @@
+/*
+  3312 - Parameters
+  -------
+  by midorizemi (@midorizemi) #简单 #infer #tuple #built-in
+
+  ### 题目
+
+  实现内置的 Parameters<T> 类型，而不是直接使用它，可参考[TypeScript官方文档](https://www.typescriptlang.org/docs/handbook/utility-types.html#parameterstype)。
+
+  例如：
+
+  ```ts
+  const foo = (arg1: string, arg2: number): void => {}
+
+  type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/3312/zh-CN
+*/
+
+
+/* _____________ 你的代码 _____________ */
+
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer R) => any ? R : never
+
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const foo = (arg1: string, arg2: number): void => {}
+const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
+const baz = (): void => {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
+
+
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/3312/answer/zh-CN
+  > 查看解答：https://tsch.js.org/3312/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
+
