551dbcde69a545b725cfce37c0d627d5cc1593dc
Seif Wu <seif.wuxf@gmail.com>
2022-08-19 10:28:35 +0800

    feat: add type-challenges 898-easy-includes

---
+++ type-challenges/src/898-easy-includes.ts
@@ -0,0 +1,64 @@
+/*
+  898 - Includes
+  -------
+  by null (@kynefuk) #简单 #array
+
+  ### 题目
+
+  在类型系统里实现 JavaScript 的 `Array.includes` 方法，这个类型接受两个参数，返回的类型要么是 `true` 要么是 `false`。
+
+  例如：
+
+  ```ts
+  type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/898/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+
+type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
+
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
+  ? MyEqual<F, U> extends true
+    ? true
+    : Includes<R, U>
+  : false;
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">, true>
+  >,
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: "A" }], { readonly a: "A" }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: "A" }], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>
+];
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/898/answer/zh-CN
+  > 查看解答：https://tsch.js.org/898/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
